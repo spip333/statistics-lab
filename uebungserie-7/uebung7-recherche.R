5b80391e49442f2a2d9a52d762b483cde5044c68
(U115136) <nicolas.stern@mobi.ch>
2018-12-10 10:13:37 +0100

    added r script recherche uebung 7

---
+++ uebungserie-7/uebung7-recherche.R
@@ -0,0 +1,178 @@
+############################################################################
+### Deskriptive Statistik - Uebungserie 7: Stetige Verteilungen
+###
+### Nicolas Stern
+### 17.11.2018
+###
+#############################################################################
+# Uebungserie 7 - Trace
+
+
+
+xv <- seq(0,5,length=100)
+plot(xv, dunif(xv,1,3), type = "l", ylab = "Uni(x)", xlab = "x")
+
+dunif(1:15, 5, 10)
+
+
+# Aufgabe: Exponentialverteilung
+# In einer vierwöchigen Datenerhebung missen Sie die Länge der Telefongespräche, 
+# die Sie auf Ihrem Handy führen. Sie finden heraus, dass die Dauer der Gespräche (in Minuten) 
+# einer Exponentialverteilung folgt, und Ihre Gespräche im Erwartungswert 3 Minuten lang sind
+# Aufgabe: 
+# 1. Welche Verteilung hat die Zufallsvariable X, welche die Dauer der Telefongespräche in Minuten beschreibt?
+# 2. Das Telefon klingelt. Wie gross ist die Wahrscheinlichkeit, dass dieses Gespräch höchstens eine Minute dauert?
+# 3. Wie gross ist die Wahrscheinlichkeit, dass das Gespräch länger als eine Minute dauert?
+# 4. Mit welcher Wahrscheinlichkeit dauert das Gespräch zwischen einer und drei Minuten?
+# 5. Berechnen und interpretieren Sie das 25%-Quantil dieser Verteilung.
+
+
+xv <- seq(1/3, 10, length=30)
+plot(xv, dexp(xv, rate=1/3), type = "l", ylab = "Exp(x)", xlab = "x")
+
+
+x<-pexp(3, rate=1/3)
+quantile(x)
+
+
+?quantile
+
+xvalues <- seq(0, 10, length=11)
+prob <- dexp(xvalues, rate=1/3)
+quantile (prob, probs=0.25 )
+
+# Aufgabe: Normalverteilung
+# Aufgabe: In einer Fabrik werden Tüten mit Kartoffelchips befüllt. Das durchschnittliche 
+# Gewicht der Tüten soll nach den Angaben des Werkes 200 g betragen. Da die Tüten 
+# maschinell befüllt werden, wird dieser Wert nur mit einer Standardabweichung 
+# von 4 g eingehalten. Mit welcher Wahrscheinlichkeit werden Tüten abgefüllt, deren 
+# Gewicht 
+# - um weniger als 2 g vom Mittelwert abweicht?
+# - über 205 g liegt?
+# - Welches Gewicht wird von 95% der Tüten überschritten?
+plot(dnorm(190:210, mean=200, sd=4), type="l")
+pnorm(204, mean=200, sd=4, lower.tail=T) - pnorm(196, mean=200, sd=4, lower.tail=T)
+
+
+?dnorm
+
+
+###########################################################
+# Normalverteilung Beispiel	
+# Funktionsdauer	von	Taschenrechnern
+# Die	Funktionsdauer	x	ist	normalverteilt	mit	Erwartungswert	µ = 120	h	
+# und	 theoretischer	Varianz s2 = 100.	
+# Wie	wahrscheinlich	ist	es,	dass	die	Funktionsdauer höchstens	135	h dauert
+pnorm(q = 135, mean = 120, sd = 10)
+
+# loesungsweg via standardiesierung
+z <- (135-120) / 10
+pnorm (q = 1.5, mean = 0, sd = 1)
+# n.b. : mean und sd sind vorbelegt. Früher haette man aus tabelle diese Werte gelesen
+# Um die"Wahrscheinlichkeit, dass die Dauer hoeher ist als x" :  
+# 1 - n oder lower tail verwenden
+
+# Rechnen	mit	der	Normalverteilung, dass	Wahrscheinlichkeit zwischen	105	und	135	Stunden	liegt:
+pnorm (q= 135, mean = 120, sd= 10) - pnorm(q = 105, mean=120, sd=10)
+
+# wleche lebensdauer wird von 95 % der TR nicht ueberschritten?
+qnorm (p = 0.95, mean = 120, sd = 10)
+
+# symmetreischer 95 %  - Bereich:
+# untere Grenze  
+qnorm (p = 0.025, mean = 120, sd = 10)
+# obere Grenze  
+qnorm (p = 0.975, mean = 120, sd = 10)
+
+# Aufgabe: Chi-Quadrat-Verteilung
+# Aufgabe: Mit welcher Wahrscheinlichkeit liegt der Wert einer χ2-Verteilung mit df = 11 über 15?
+
+
+###########################################################
+# Aufgabe: Chi-Quadrat-Verteilung
+# Mit welcher Wahrscheinlichkeit liegt der Wert einer Chi-quadrat-Verteilung  mit df = 11 über 15?
+1-pchisq(15, df=11)
+
+qchisq(.8, df=10, lower.tail = T)
+qchisq(.8, df=10, lower.tail = F)
+qchisq(.2, df=10, lower.tail = T)
+qchisq(.2, df=10, lower.tail = F)
+
+
+x1 <- seq(0.5, 0.95, .05 )
+y1 <- qchisq(x1, df=11)
+v1 <- matrix(c(x1, y1), nrow = 10, byrow = FALSE)
+
+x2 <- seq(0.8, 0.85, .01 )
+y2 <- qchisq(x2, df=11)
+v2 <- matrix(c(x2, y2), nrow = 6, byrow = FALSE)
+
+v2
+
+length(x)
+
+qchisq(.000000000000005, df=3, lower.tail = F)
+
+plot(qchisq())
+
+?qchisq
+
+# exemples chi-2
+# Bestimmen Sie das 95%-Perzentil der Chi-2-Verteilung mit Freiheitsgrad 7.
+qchisq(.95, df=7)
+
+vec <- seq(.05, .9, .01)
+
+plot(qchisq(vec, df=11, lower.tail = F), type = "l")
+plot(qchisq(vec, df=11), type = "l")
+
+vec <- seq(.8, .9, .010)
+
+plot(qchisq(vec, df=11))
+
+     
+
+plot(dchisq(1:20, df=1), type="l")
+
+plot(dchisq(1:50, df=10), type="l")
+plot(pchisq(1:20, df=10), type="l")
+plot(dchisq(1:20, df=3))
+
+
+# bsps
+plot(dchisq(1:30, df=11), type="l")
+?dchisq
+
+# Studentsche t-Verteilung
+dt(0, df=5 )
+pt(2.3, df=5)
+qt(.05, df=46)
+
+vec <- seq(-10, 10, .1)
+plot(dt(vec, df=5), type="l")
+
+
+# Aufgabe: Mit welcher Wahrscheinlichkeit liegt der Wert der Studentschen t-Verteilung 
+# unter -0.5, respektive unter 1? Der Freiheitsgrad sei 7.
+pt(-0.5, df=7)
+pt(1, df=7)
+
+# Bestimmen Sie das 2:5%- und das 97:5%-Perzentil der Studentschen t-Verteilung mit Freiheitsgrad 5.
+qt(c(0.025, 0.975), df=5)
+
+
+# Schiefe
+# Bestimmen Sie die Schiefe der Wartezeiten von Faithful.
+faithful
+summary(faithful)
+wait <- faithful$waiting
+y <- sort(wait)
+
+library(e1071)
+skewness(waiting)
+## [1] -0.4135498
+x <- 1:length(wait)
+
+plot(x, y, type="l")
+frequency(wait)
+?frequency
