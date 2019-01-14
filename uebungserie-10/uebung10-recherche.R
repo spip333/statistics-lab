7aee4ed37d8430bc06b15ea46bcb8d8acfebbd80
(U115136) <nicolas.stern@mobi.ch>
2019-01-14 10:17:57 +0100

    wip uebung 10

--- uebungserie-10/uebung10-recherche.R
+++ uebungserie-10/uebung10-recherche.R
@@ -2,16 +2,36 @@
 # Recherche Uebung 10
 ###########################
 
-eruption.lm <- lm(eruptions~waiting , data = faithful)
-
 ?lm
 
 head(mtcars)
 
+y <- mtcars$wt
+x <- mtcars$hp
+plot(x, y)
+
+
 # model wt as function der hp
-model <- lm (wt~hp, data=mtcars)
-model$coefficients
+wt.hp.model <- lm (wt~hp, data=mtcars)
+wt.hp.model$coefficients
+
+coeffs <- coefficients(wt.hp.model)
+coeffs
+est.wt <- coeffs[1]+ 200 * coeffs[2] 
+est.wt
+# gewicht bei 200hp:
+coeffs[1]+ 200 * coeffs[2] 
+
+newdata <- data.frame(hp=200)
+
+predict(wt.hp.model,newdata)
+
+hps <- c(110)
+
+newdata <- data.frame(hp=hps)
+predict(wt.hp.model,newdata)
+
+head(mtcars)
 
 
-waiting <- faithful$waiting
-
+model$fitted.values
