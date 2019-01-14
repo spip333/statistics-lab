3697d044bed7ffe9ab206c7a540b77b6410236a8
(U115136) <nicolas.stern@mobi.ch>
2019-01-14 22:27:31 +0100

    wip uebung 10

--- uebungserie-10/uebung10-recherche.R
+++ uebungserie-10/uebung10-recherche.R
@@ -15,12 +15,17 @@ plot(x, y)
 wt.hp.model <- lm (wt~hp, data=mtcars)
 wt.hp.model$coefficients
 
+plot(wt.hp.model)
+
+
 coeffs <- coefficients(wt.hp.model)
 coeffs
 est.wt <- coeffs[1]+ 200 * coeffs[2] 
 est.wt
 # gewicht bei 200hp:
 coeffs[1]+ 200 * coeffs[2] 
+
+paste("a", "b", "c")
 
 newdata <- data.frame(hp=200)
 
@@ -35,3 +40,5 @@ head(mtcars)
 
 
 model$fitted.values
+
+
