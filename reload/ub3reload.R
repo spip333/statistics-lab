08af708379662b43ec04c8d6df6841b100c0a95f
(U115136) <nicolas.stern@mobi.ch>
2019-01-17 17:44:55 +0100

    ub3reload added

--- reload/ub3reload.R
+++ reload/ub3reload.R
@@ -13,4 +13,25 @@ head(Daten_Wachstum)
 head(Daten_Wachstum$Motiv)
 head(Daten_Wachstum$Geschlecht)
 
+tab = table(Daten_Wachstum$Geschlecht, Daten_Wachstum$Motiv)
+tab
+
+# Aufgabe: Randverteilungen
+# Aufgabe: Fügen Sie die Randverteilungen zur zweidimensionalen Häuﬁgkeitsverteilung
+# der Merkmale Geschlecht und Motiv hinzu.
+addmargins(tab)
+
+# Aufgabe: Relative Zweidimensionale Verteilung
+# Aufgabe: Bestimmen Sie die relative zweidimensionale Häuﬁgkeitsverteilung der Merkmale 
+# Geschlecht und Motiv.
+prop.table(tab)
+
+# Aufgabe: Bedingte Verteilung 1
+# Aufgabe: Wie verteilen sich die verschiedenen Motive innerhalb der Geschlechtergruppen
+addmargins(prop.table(tab, 1))
+
+# Aufgabe: Bedingte Verteilung 2
+# Aufgabe: Wie verteilen sich die beiden Geschlechter auf die verschiedenen Motive?
+addmargins(prop.table(tab, 2))
+
 
